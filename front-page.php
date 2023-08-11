6a6c81d28d83b0146c66b2504b4bce70ee52c5bd
nburd01 <n.burdon@protonmail.com>
2023-08-11 12:05:04 +0200

    hero ok on desktop

--- front-page.php
+++ front-page.php
@@ -4,8 +4,7 @@
 
 <?php get_template_part('template-parts/nav'); ?>
 <?php get_template_part('template-parts/hero'); ?>
-<?php get_template_part('template-parts/about'); ?>
-<?php get_template_part('template-parts/numbers'); ?>
+
 
 
 
