3e4da993a3a68c2d1f9ab0b631fe5d30c089d638
nburd01 <n.burdon@protonmail.com>
2023-08-12 10:25:22 +0200

    testing deleting sass

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,6 +1,6 @@
 <div class="container">
-<div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-  <img src="<?php echo get_template_directory_uri(); ?>/resources/images/jerome/DSC_8796.png" alt="Desktop Homepage" alt="" class="absolute opacity-50 inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+<div class="relative isolate overflow-hidden bg-gray-900 sm:py-24 my-4 sm:py-32">
+  <img src="<?php echo get_template_directory_uri(); ?>/resources/images/jeromekeepers/DSC_1524.jpg" alt="Desktop Homepage" alt="" class="absolute bg-blue-900 opacity50 inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
   <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
     <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
   </div>
@@ -9,7 +9,7 @@
   </div>
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:mx-0">
-      <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Work with us</h2>
+      <h2 class="text-4xl mt-4 font-bold tracking-tight text-white sm:text-4xl sm:mt-4 ">Work with us</h2>
       <p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
     </div>
     <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
