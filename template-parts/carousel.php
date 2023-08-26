5bd9b4259af8895ec6f767089761a94eb847ce96
nburd01 <n.burdon@protonmail.com>
2023-08-26 17:28:18 +0200

    functioning

---
+++ template-parts/carousel.php
@@ -0,0 +1,6 @@
+
+<div class="container-large bg-indigo-600 px-3.5 py-2.5 ">
+    <div class="container">
+        <?php echo do_shortcode('[sp_wpcarousel id="16"]'); ?>
+    </div>
+</div>
---
+++ template-parts/contact.php
@@ -0,0 +1,3 @@
+<div class="container">
+    <?php echo do_shortcode('[contact-form-7 id="9abc6ea" title="Formulaire de contact 1"]'); ?>
+</div>
